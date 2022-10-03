3c4b7a052d35a285d9c13edd475761bc41eab709
Brendan Molloy <brendan@bbqsrc.net>
2022-10-03 12:25:21 +0200

    Collections don't need to be iterable

--- src/collection.rs
+++ src/collection.rs
@@ -1,6 +1,4 @@
-use crate::{iter::IterableMut, Iterable};
-
-pub trait Collection<T>: Iterable {
+pub trait Collection<T> {
     fn len(&self) -> usize;
     fn is_empty(&self) -> bool {
         self.len() == 0
@@ -10,6 +8,6 @@ pub trait Collection<T>: Iterable {
         T: PartialEq;
 }
 
-pub trait CollectionMut<T>: Collection<T> + IterableMut {
+pub trait CollectionMut<T>: Collection<T> {
     fn clear(&mut self);
 }
